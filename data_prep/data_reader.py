6fc96d18f11f9324da62eea7046160dfc571142d
Mai Hassanin <maielbaradei@gmail.com>
2024-03-28 08:29:46 +0200

    error handeling for data reading functions

--- data_prep/data_reader.py
+++ data_prep/data_reader.py
@@ -13,11 +13,11 @@ def from_excel(path: str) -> Union[pd.Dataframe, None]:
     try:
         return pd.read_excel(path)
     except ExceptionType as e:
-        print(f"Error reading EXcel file: {e}")
+        print(f"Error reading Excel file: {e}")
 
 
 def from_json(path: str) -> Union[pd.Dataframe, None]:
     try:
         return pd.read_json(path)
     except ExceptionType as e:
-        print(f"Error reading CSV file: {e}")
+        print(f"Error reading JSON file: {e}")

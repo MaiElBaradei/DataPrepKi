1f2e8e7db08ea4237d6852f29f4c47593b7bca1a
Mai Hassanin <maielbaradei@gmail.com>
2024-03-28 08:29:12 +0200

    error handeling for data reading functions

--- data_prep/data_reader.py
+++ data_prep/data_reader.py
@@ -1,13 +1,23 @@
 import pandas as pd
+from typing import Union
 
 
-def from_csv(path: str) -> pd.Dataframe:
-    return pd.read_csv(path)
+def from_csv(path: str) -> Union[pd.Dataframe, None]:
+    try:
+        return pd.read_csv(path)
+    except ExceptionType as e:
+        print(f"Error reading CSV file: {e}")
 
 
-def from_excel(path: str) -> pd.Dataframe:
-    return pd.read_excel(path)
+def from_excel(path: str) -> Union[pd.Dataframe, None]:
+    try:
+        return pd.read_excel(path)
+    except ExceptionType as e:
+        print(f"Error reading EXcel file: {e}")
 
 
-def from_json(path: str) -> pd.DataFrame:
-    return pd.read_json(path)
+def from_json(path: str) -> Union[pd.Dataframe, None]:
+    try:
+        return pd.read_json(path)
+    except ExceptionType as e:
+        print(f"Error reading CSV file: {e}")

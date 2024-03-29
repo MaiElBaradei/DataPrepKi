3bc6627f8a8ccbcd50b9884230a29aa149f9e8c5
Mai Hassanin <maielbaradei@gmail.com>
2024-03-29 02:21:31 +0200

    setup file

---
+++ sestup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='DataPrepKit',
+    version='1.0',
+    packages=find_packages(),
+    install_requires=['pandas'],
+    author='Mai Hassanin ElBaradei',
+    description='A Python package for data preprocessing',
+    url='https://github.com/MaiElBaradei/DataPrepKit',
+)

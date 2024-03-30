05a93d62f1e7e4495deb66e320b8610573d74d56
Mai Hassanin <maielbaradei@gmail.com>
2024-03-30 08:03:12 +0200

    remove min,max

--- DataPrepKit/__init__.py
+++ DataPrepKit/__init__.py
@@ -1,4 +1,4 @@
-from .data_summary import summary, count, max, min, mean, median, mode
+from .data_summary import summary, count, mean, median, mode
 from .data_reader import from_csv, from_excel, from_json
 from .missing_values import remove_missing, impute_missing
 from .categorical_encoding import one_hot_encoding, label_encoding

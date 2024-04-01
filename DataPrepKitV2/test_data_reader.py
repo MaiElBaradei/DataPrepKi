048ed8fba8f829b23caed4ce29b07587aeb76ba1
Mai Hassanin <maielbaradei@gmail.com>
2024-04-01 17:57:07 +0200

    replace dataprepkit with dataprepV2

--- DataPrepKitV2/test_data_reader.py
+++ DataPrepKitV2/test_data_reader.py
@@ -4,15 +4,15 @@ from .data_reader import from_csv, from_excel, from_json
 
 
 def test_from_csv():
-    df = from_csv("DataPrepKit/winemag-data-130k-v2.csv")
+    df = from_csv("DataPrepKitV2/winemag-data-130k-v2.csv")
     assert isinstance(df, pd.DataFrame)
 
 
 def test_from_excel():
-    df = from_excel("DataPrepKit/winemag-data-130k-v2.xlsx")
+    df = from_excel("DataPrepKitV2/winemag-data-130k-v2.xlsx")
     assert isinstance(df, pd.DataFrame)
 
 
 def test_from_json():
-    df = from_json("DataPrepKit/iris.json")
+    df = from_json("DataPrepKitV2/iris.json")
     assert isinstance(df, pd.DataFrame)
--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='DataPrepKit',
+    name='DataPrepKitV2',
     version='2.0',
     packages=find_packages(),
     install_requires=['pandas'],
     author='Mai Hassanin ElBaradei',
     description='A Python package for data preprocessing',
-    url='https://github.com/MaiElBaradei/DataPrepKit',
+    url='https://github.com/MaiElBaradei/DataPrepKitV2',
 )

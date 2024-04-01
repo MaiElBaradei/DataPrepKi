2c719e2427c476888e94d0b695a55da3afeaefc9
Mai Hassanin <maielbaradei@gmail.com>
2024-04-01 12:44:52 +0200

    change version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='DataPrepKit',
-    version='1.0',
+    version='2.0',
     packages=find_packages(),
     install_requires=['pandas'],
     author='Mai Hassanin ElBaradei',
